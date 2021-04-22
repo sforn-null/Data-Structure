e7ef0d9a103dd0a233adec661034e40165d1398d
sforn-null <1641832926@qq.com>
2021-04-22 14:37:00 +0800

    队列的源文件

---
+++ Queue.c
@@ -0,0 +1,90 @@
+#include "Queue.h"
+
+void QueueInit(Queue* pq)
+{
+	assert(pq);
+	pq->_head = pq->_tail = NULL;
+}
+
+void QueueDestory(Queue* pq)
+{
+	QueueNode* cur = pq->_head;
+	while (cur)
+	{
+		QueueNode* next = cur->_next;
+		free(cur);
+		cur = next;
+	}
+
+	pq->_head = pq->_tail = NULL;
+}
+
+void Queuepush(Queue* pq, QDataType x)
+{
+	assert(pq);
+	QueueNode* newnode = (QueueNode*)malloc(sizeof(QueueNode));
+	if (newnode == NULL)
+	{
+		printf("ÄÚ´æ²»×ã");
+		exit(-1);
+	}
+	newnode->_data = x;
+	newnode->_next = NULL;
+	if (pq->_head == NULL)
+	{
+		pq->_head = pq->_tail = newnode;
+	}
+	else
+	{
+		pq->_tail->_next = newnode;
+		pq->_tail = newnode;
+	}
+}
+
+void QueuePop(Queue* pq)
+{
+	assert(pq);
+	assert(pq->_head);
+	QueueNode* next = pq->_head->_next;
+	free(pq->_head);
+	pq->_head = next;
+	if (pq->_head == NULL)
+	{
+		pq->_tail = NULL;
+	}
+}
+
+QDataType QueueFront(Queue* pq)
+{
+	assert(pq);
+	assert(pq->_head);
+
+	return pq->_head->_data;
+}
+
+QDataType QueueBack(Queue* pq)
+{
+	assert(pq);
+	assert(pq->_tail);
+
+	return pq->_tail->_data;
+}
+
+int QueueEmpty(Queue* pq)
+{
+	assert(pq);
+	return pq->_head == NULL ? 1 : 0;
+}
+
+int QueueSize(Queue* pq)
+{
+	assert(pq);
+	int size = 0;
+	QueueNode* cur = pq->_head;
+	while (cur)
+	{
+		size++;
+		cur = cur->_next;
+	}
+	return size;
+}
